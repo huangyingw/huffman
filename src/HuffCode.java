9f2938814a8a38188c19f6db8fa21dbc350cfc9a
huangyingw@linux_programming <huangyingw@gmail.com>
2012-07-24 16:36:15 -0500

    n

--- src/HuffCode.java
+++
@@ -1,4 +0,0 @@
-public class HuffCode {
-	public char[] cd;
-	public int start;
-}
--- src/HuffNode.java
+++
@@ -1,7 +0,0 @@
-public class HuffNode {
-	public char data;
-	public int weight;
-	public int parent;
-	public int left;
-	public int right;
-}
