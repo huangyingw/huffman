532c2982fa25d5fe8ae73e3465cced2cc5bc3bac
huangyingw@linux_programming <huangyingw@gmail.com>
2012-08-06 22:37:45 -0500

    n

--- src/nayuki/huffmancoding/Huffman.java
+++ src/nayuki/huffmancoding/Huffman.java
@@ -219,15 +219,12 @@ public class Huffman {
 
 		LinkedHashMap<Character, Integer> hasmap = new LinkedHashMap<Character, Integer>();
 		hasmap.put('a', 4);
-		hasmap.put('b', 5);
+		hasmap.put('b', 6);
 		hasmap.put('c', 8);
 		hasmap.put('d', 10);
+		hasmap.put('e', 1);
 
 		Huffman huffman = new Huffman(hasmap);
-		String temp = huffman.enCodeString("abcd");
-		System.out.println(temp);
-		System.out.println(huffman.deCodeString(temp));
-
 	}
 
 }
