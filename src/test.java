33912d7ffb08e4fcff630a1abb0ab19894e111cd
huangyingw@linux_programming <huangyingw@gmail.com>
2012-08-06 19:09:33 -0500

    n

--- src/test.java
+++ src/test.java
@@ -5,14 +5,16 @@ public class test {
 
 		LinkedHashMap<Character, Integer> hasmap = new LinkedHashMap<Character, Integer>();
 		hasmap.put('a', 4);
-		hasmap.put('b', 5);
+		// hasmap.put('b', 5);
+		hasmap.put('b', 6);
 		hasmap.put('c', 8);
 		hasmap.put('d', 10);
+		hasmap.put('e', 1);
 
 		Huffman huffman = new Huffman(hasmap);
-		String temp = huffman.enCodeString("abcd");
-		System.out.println(temp);
-		System.out.println(huffman.deCodeString(temp));
+		// String temp = huffman.enCodeString("abcd");
+		// System.out.println(temp);
+		// System.out.println(huffman.deCodeString(temp));
 
 	}
 
